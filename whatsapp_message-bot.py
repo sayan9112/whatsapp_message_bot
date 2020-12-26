fabb556601824b89aab0dc87a573ad835619da2d
Sayan Mukherjee <70065597+sayan9112@users.noreply.github.com>
2020-12-26 17:50:39 +0600

    Update whatsapp_message-bot.py

--- whatsapp_message-bot.py
+++ whatsapp_message-bot.py
@@ -1,2 +1,6 @@
 import pywhatkit
 pywhatkit.sendwhatmessage('receiver whatsapp number', 'the message we want to send')
+#author: Sayan Mukherjee (kalbhairab)
+#mail: almighty.kalbhairab@gmail.com
+#linkedIn: https://www.linkedin.com/in/sayan-mukherjee-7845421a7/
+#github: github.com/sayan9112 (Personal Website)
